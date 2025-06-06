c0e9c00a2c7fcae3b75992ea9a905cdd1229c1bd
Paula Dias Calderaro <pauladias.web@gmail.com>
2025-06-06 20:01:01 -0300

    first commit

---
+++ switch_case.js
@@ -0,0 +1,47 @@
+/*const numero = prompt ("Digite um número");
+
+switch (+numero){
+
+case 1: 
+console.log('Domingo');
+break;
+
+case 2:
+    console.log('Segunda-feira');
+    break;
+
+case 3:
+    console.log('Terça-feira');
+    break;
+
+case 4: 
+console.log('Quarta-feira');
+break;
+
+  default:
+        console.error('Número digitado inválido');
+        break;
+} */
+
+
+        const letra = prompt ("Digite uma letra");
+
+        switch (letra){
+case 'a':
+    case 'e':
+        case 'i':
+            case 'o':
+                case 'u':
+                    break;
+                    console.log(`A letra ${letra} é uma vogal`);  
+                       
+                    
+                    default:
+                        console.log (`A letra ${letra} é uma consoante`)
+                 break;
+                   
+
+
+
+
+        }
